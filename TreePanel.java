29c239cee497eca96b4baa9208233daea507655e
Ella Chao <hchao@wellesley.edu>
2015-12-14 14:18:23 -0500

    add comments

--- TreePanel.java
+++ TreePanel.java
@@ -6,8 +6,8 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class TreePanel extends JPanel{
-  private static final int WIDTH=36;
-  private static final int LEVELDISTANCE=3;
+  private static final int WIDTH=36; //width of 2-d array
+  private static final int LEVELDISTANCE=3; //distance between each level
   private int[][] grid;
   private JLayeredPane lpane = new JLayeredPane();
   private JPanel panelStart = new JPanel();
@@ -18,9 +18,10 @@ public class TreePanel extends JPanel{
   private RandomTree tree;
   private JLabel orderLabel,messageLabel, bthumb;
   private boolean key_right, key_left, key_down, key_up, key_space; // Input booleans
-  private int x;
+  //coordinates of makar
+  private int x; 
   private int y;
-  private int temp;
+  private int temp; //value of grid[y][x]
   private AnswerQueue ans;
   private boolean added = false;
   
@@ -49,11 +50,19 @@ public class TreePanel extends JPanel{
     return tree;
   }
   private void drawTree(){
+    //list of rupee images
     String[] images={"rupee1.jpg","rupee2.jpg","rupee3.jpg","rupee4.jpg","rupee5.jpg","rupee6.jpg"};
+    //iterate through 2-d array
+    //0 for grass
+    //-1 for road
+    //-2 for makar
+    //everything else is gem
     for(int y=0;y<WIDTH;y++){
       for(int x=0;x<WIDTH;x++){
+        //use value in 2-d array to display corresponding image
         if(grid[y][x]==0){
           JLabel img =new JLabel(new ImageIcon("images/grass.jpg"));
+          //images are 30 by 30
           img.setBounds(x*30,(y+10)%WIDTH*30,30,30);
           add(img);
         }
@@ -73,10 +82,7 @@ public class TreePanel extends JPanel{
           add(img3);
         }
       }
-    //lpane.setBounds(0, 0, 600, 400);
-    //panelBackground.setBackground(Color.BLUE);
     ImageIcon background = new ImageIcon("images/panel.jpg"); 
-    //creates a label and sets the image to it
     bthumb = new JLabel();
     bthumb.setIcon(background);
     panelBackground.setBounds(460, -5, 1345, 1200);
@@ -105,17 +111,20 @@ public class TreePanel extends JPanel{
     setBackground(new Color(218,218,218));
   }
   
+  //converts list of nodes to a 2-d array 
   public int[][] convertTree(int[] intArray){
-    //0 for grass; 1 for nodes
+    //0 for grass because int is 0 by default
     int row=0;
-    int index=0;
-    int limit=0;
+    int index=0; //level
+    int limit=0; //number of nodes in level
     int power=(int)Math.pow(2,index);
-    int segment=WIDTH/(power+1);
+    int segment=WIDTH/(power+1); //distance between each node in that level
     int distance=segment;
     int[][] grid=new int[WIDTH][WIDTH];
     for (int i=0; i<intArray.length;i++){
       if(i>limit){
+        //go to next level
+        //update variables
         index++;
         row+=3;
         power=(int)Math.pow(2,index);
@@ -123,23 +132,28 @@ public class TreePanel extends JPanel{
         segment=WIDTH/(power+1);
         distance=segment;       
       }
-      if (intArray[i]>-1){
+      //if there is a node
+      if (intArray[i]>0){
+        //update element to value from intArray
         grid[row][distance]=intArray[i];
       }
+      //updates distance to position of next node
       distance+=segment;
     }
     fillPath(grid);
     return grid;
   }
   
+  //connect parent nodes to children
   public void fillPath(int[][] grid){
     //fills paths with -1
     int pX;
     int pY;
+    //loops through 2-d array but skip first level
     for(int y=LEVELDISTANCE;y<grid.length;y+=3){
       for(int x=0;x<grid[y].length;x++){
         if(grid[y][x]>=1){
-          pX=findParent(x,y);
+          pX=findParent(x,y); //x-coordinate for parenet
           pY=y-LEVELDISTANCE;
           //fills in vertical path
           for(int i=1;i<LEVELDISTANCE;i++){
@@ -149,9 +163,11 @@ public class TreePanel extends JPanel{
           if(pX!=x){
             for(int i=0;i<Math.abs(pX-x);i++){
               if(x>pX){
+                //to the left
                 grid[pY][x-i]=-1;
               }
               else{
+                //to the right
                 grid[pY][x+i]=-1;
               }
             }
@@ -172,19 +188,22 @@ public class TreePanel extends JPanel{
     return (((x/childSeg)+1)/2)*parentSeg; //returns parent's x coordinate
   }
   private void moveMakar(int shiftX, int shiftY){
+    //restore previous of where makar is right now
     grid[y][x]=temp;
+    //calculates new coordinate
     int newY=((y+shiftY)%WIDTH<0)?(WIDTH-1):(y+shiftY)%WIDTH;
     int newX=((x+shiftX)%WIDTH<0)?(WIDTH-1):(x+shiftX)%WIDTH;
-    temp=grid[newY][newX];
-    grid[newY][newX]=-2;
-    
+    temp=grid[newY][newX]; //stores value of where Makar will move to
+    grid[newY][newX]=-2; //update that postion to makar
+    //updates coordinates of makar
     x=newX;
     y=newY;
-    //System.out.println(1);
   }
   
   public void paintComponent(Graphics g) {
     //super.paintComponent(g);
+    //check which keys is pressed
+    //call moveMakar accordingly
     if (key_down) {  
       moveMakar(0,1);
       
@@ -214,13 +233,13 @@ public class TreePanel extends JPanel{
     removeAll();
     revalidate();
     drawTree();
-    //repaint(0,0,1080,1080);
     repaint();
   }
 
   private class KeyInput implements KeyListener {
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {} //abstract method that has to be implemented
     public void keyReleased(KeyEvent e) {
+      //checks key and updates instance variables accordingly
       if (e.getKeyCode() == e.VK_DOWN) key_down = false;
       if (e.getKeyCode() == e.VK_UP) key_up = false;
       if (e.getKeyCode() == e.VK_RIGHT) key_right = false;
